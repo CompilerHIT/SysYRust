2aee6c4d0ef4f71243b2a3184b14a83697140537
Sakura1609 <3078688722@qq.com>
2023-04-13 18:24:44 +0800

    move tasks to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 mod backend;
 mod ir;
 mod utility;
+
+// TODO: to add call for generate and new module.
+// for example: asm_module = AsmModule::new(ir_module);
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,3 @@ fn main() {
     println!("Hello, world!");
 }
 
-// TODO: to add call for generate and new module.
