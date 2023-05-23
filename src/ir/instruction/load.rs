ca0ec97a8cbcf6e755081b975e0e6225a6d26776
LJZ <jz.long@qq.com>
2023-05-23 14:26:49 +0800

    修改load的代码结构，去掉本没有的offset

--- src/ir/instruction/load.rs
+++ src/ir/instruction/load.rs
@@ -8,24 +8,20 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_int_load(&mut self, ptr: ObjPtr<Inst>, offset: ObjPtr<Inst>) -> ObjPtr<Inst> {
+    pub fn make_int_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IntPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_int_load: offset must be a int"),
-            },
+            IntPtr => {}
             _ => unreachable!("ObjPool::make_int_load: ptr must be a pointer"),
         }
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Int,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -36,30 +32,22 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_global_int_load(
-        &mut self,
-        ptr: ObjPtr<Inst>,
-        offset: ObjPtr<Inst>,
-    ) -> ObjPtr<Inst> {
+    pub fn make_global_int_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
             // 全局变量作为指针，但是其值的类型仍为Int
-            IrType::Int | IrType::ConstInt => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_global_int_load: offset must be a int"),
-            },
+            IrType::Int | IrType::ConstInt => {}
             _ => unreachable!("ObjPool::make_global_int_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Int,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -69,25 +57,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_int_array_load(&mut self, ptr: ObjPtr<Inst>, offset: ObjPtr<Inst>) -> ObjPtr<Inst> {
+    pub fn make_int_array_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IntPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_int_array_load: offset must be a int"),
-            },
+            IntPtr => {}
             _ => unreachable!("ObjPool::make_int_array_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Int,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -98,29 +82,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_global_int_array_load(
-        &mut self,
-        ptr: ObjPtr<Inst>,
-        offset: ObjPtr<Inst>,
-    ) -> ObjPtr<Inst> {
+    pub fn make_global_int_array_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IrType::IntPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_global_int_array_load: offset must be a int"),
-            },
+            IrType::IntPtr => {}
             _ => unreachable!("ObjPool::make_global_int_array_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::IntPtr,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -130,25 +106,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_float_load(&mut self, ptr: ObjPtr<Inst>, offset: ObjPtr<Inst>) -> ObjPtr<Inst> {
+    pub fn make_float_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IrType::FloatPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_float_load: offset must be a int"),
-            },
+            IrType::FloatPtr => {}
             _ => unreachable!("ObjPool::make_float_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Float,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -159,29 +131,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_global_float_load(
-        &mut self,
-        ptr: ObjPtr<Inst>,
-        offset: ObjPtr<Inst>,
-    ) -> ObjPtr<Inst> {
+    pub fn make_global_float_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IrType::Float | IrType::ConstFloat => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_global_float_load: offset must be a int"),
-            },
+            IrType::Float | IrType::ConstFloat => {}
             _ => unreachable!("ObjPool::make_global_float_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Float,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -192,29 +156,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_float_array_load(
-        &mut self,
-        ptr: ObjPtr<Inst>,
-        offset: ObjPtr<Inst>,
-    ) -> ObjPtr<Inst> {
+    pub fn make_float_array_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IrType::FloatPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_float_array_load: offset must be a int"),
-            },
+            IrType::FloatPtr => {}
             _ => unreachable!("ObjPool::make_float_array_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::Float,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -225,29 +181,21 @@ impl ObjPool<Inst> {
     /// * 'offset' - 偏移量
     /// # Return
     /// 返回一个Inst实例
-    pub fn make_global_float_array_load(
-        &mut self,
-        ptr: ObjPtr<Inst>,
-        offset: ObjPtr<Inst>,
-    ) -> ObjPtr<Inst> {
+    pub fn make_global_float_array_load(&mut self, ptr: ObjPtr<Inst>) -> ObjPtr<Inst> {
         // 正确性检查
         match ptr.as_ref().get_ir_type() {
-            IrType::FloatPtr => match offset.as_ref().get_ir_type() {
-                IrType::Int | IrType::ConstInt => {}
-                _ => unreachable!("ObjPool::make_global_float_array_load: offset must be a int"),
-            },
+            IrType::FloatPtr => {}
             _ => unreachable!("ObjPool::make_global_float_array_load: ptr must be a pointer"),
         }
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::FloatPtr,
             InstKind::Load,
-            vec![ptr, offset],
+            vec![ptr],
         ));
 
         // 设置use list
         ptr.as_mut().add_user(inst.as_ref());
-        offset.as_mut().add_user(inst.as_ref());
 
         inst
     }
@@ -261,13 +209,6 @@ impl Inst {
         self.user.get_operand(0)
     }
 
-    /// 获得偏移量
-    /// # Return
-    /// 返回偏移量的引用
-    pub fn get_offset(&self) -> ObjPtr<Inst> {
-        self.user.get_operand(1)
-    }
-
     /// 修改指针
     /// # Arguments
     /// * 'ptr' - 新的指针
@@ -277,18 +218,5 @@ impl Inst {
         ptr.as_mut().add_user(self);
 
         self.user.set_operand(0, ptr);
-    }
-
-    /// 修改偏移量
-    /// # Arguments
-    /// * 'offset' - 新的偏移量
-    pub fn set_offset(&mut self, offset: ObjPtr<Inst>) {
-        // 正确性检查
-        match offset.as_ref().get_ir_type() {
-            IrType::Int | IrType::ConstInt => {}
-            _ => unreachable!("Inst::set_offset: offset must be a int"),
-        }
-
-        self.user.set_operand(1, offset);
     }
 }
