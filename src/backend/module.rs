5703bc0707270f7bac294bd73161b37caa49f240
LJZ <jz.long@qq.com>
2023-06-04 14:40:37 +0800

    修改了后端对于module引用的生命周期需求

--- src/backend/module.rs
+++ src/backend/module.rs
@@ -2,29 +2,27 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 
-use crate::ir::module::Module;
-use crate::ir::instruction:: InstKind;
-use crate::ir::function::Function;
-use crate::backend::structs::{IGlobalVar, FGlobalVar, GlobalVar};
 use crate::backend::func::Func;
 use crate::backend::operand::ToString;
-use crate::utility::{ObjPtr, ObjPool};
-
+use crate::backend::structs::{FGlobalVar, GlobalVar, IGlobalVar};
+use crate::ir::function::Function;
+use crate::ir::instruction::InstKind;
+use crate::ir::module::Module;
+use crate::utility::{ObjPool, ObjPtr};
 
 use super::structs::GenerateAsm;
 
-
-pub struct AsmModule {
+pub struct AsmModule<'a> {
     global_var_list: Vec<GlobalVar>,
 
     // const_array_mapping: HashMap<String, ArrayConst>,
     func_map: Vec<(ObjPtr<Function>, ObjPtr<Func>)>,
-    pub upper_module: &'static Module,
+    pub upper_module: &'a Module,
     func_mpool: ObjPool<Func>,
 }
 
-impl AsmModule {
-    pub fn new(ir_module: &'static Module) -> Self {
+impl<'a> AsmModule<'a> {
+    pub fn new(ir_module: &'a Module) -> Self {
         let global_var_list = Self::get_global(ir_module);
         Self {
             global_var_list,
@@ -34,7 +32,7 @@ impl AsmModule {
             func_mpool: ObjPool::new(),
         }
     }
-    
+
     pub fn build_lir(&mut self) {
         let mut func_seq = 0;
         for (name, iter) in &self.upper_module.function {
@@ -76,17 +74,14 @@ impl AsmModule {
         for (name, iter) in map {
             //TODO: update ir translation，to use ObjPtr match
             match iter.as_ref().get_kind() {
-                InstKind::GlobalConstInt(value) => 
-                    list.push(GlobalVar::IGlobalVar(
-                        IGlobalVar::init(name.to_string(), value, true)
-                    )),
-                InstKind::GlobalConstFloat(value) => 
-                    list.push(GlobalVar::FGlobalVar(
-                        FGlobalVar::init(name.to_string(), value, true)
-                    )),
+                InstKind::GlobalConstInt(value) => list.push(GlobalVar::IGlobalVar(
+                    IGlobalVar::init(name.to_string(), value, true),
+                )),
+                InstKind::GlobalConstFloat(value) => list.push(GlobalVar::FGlobalVar(
+                    FGlobalVar::init(name.to_string(), value, true),
+                )),
                 _ => panic!("fail to analyse GlobalConst"),
             };
-            
         }
         list
     }
@@ -97,7 +92,7 @@ impl AsmModule {
                 GlobalVar::IGlobalVar(ig) => {
                     let name = ig.get_name();
                     let value = ig.get_init().to_string();
-                    writeln!(f , "{name}:\n        .word:   {value}\n");
+                    writeln!(f, "{name}:\n        .word:   {value}\n");
                     writeln!(f, "    {value}\n");
                 }
                 GlobalVar::FGlobalVar(fg) => {
@@ -111,7 +106,8 @@ impl AsmModule {
 
     fn generate_asm(&mut self, f: &mut File) {
         self.func_map.iter_mut().for_each(|(_, func)| {
-            func.as_mut().generate(ObjPtr::new(&crate::backend::structs::Context::new()), f);
+            func.as_mut()
+                .generate(ObjPtr::new(&crate::backend::structs::Context::new()), f);
         });
     }
 }
