888feab790c4e490da58c30e21ddd6d5b101a3a0
Dak <1025707920@qq.com>
2023-06-04 12:59:55 +0800

    formatted

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,9 +1,9 @@
-pub mod context;
 pub mod ast;
+pub mod context;
 pub mod error;
 pub mod irgen;
 
-#[derive(Debug,Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 pub enum ExpValue {
     Float(f32),
     Int(i32),
