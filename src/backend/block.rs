3122e01f0c810658b599a810004b93f051fdeb2f
XMky <3078688722@qq.com>
2023-06-06 17:46:30 +0800

    恢复

--- src/backend/block.rs
+++ src/backend/block.rs
@@ -399,7 +399,7 @@ impl BB {
                         vec![value_reg, addr_reg, Operand::IImm(IImm::new(0))],
                     )));
                 }
-                InstKind::Alloca => {
+                InstKind::Alloca(size) => {
                     let array_num = get_current_array_num();
                     let label = format!(".LC{array_num}");
                     inc_array_num();
@@ -408,7 +408,6 @@ impl BB {
                     //TODO:la dst label    sd dst (offset)sp
                     //TODO: 大数组而装填因子过低的压缩问题
                     //FIXME: 认为未初始化数组也被初始化为全0
-                    let size = inst_ref.get_array_length().as_ref().get_int_bond();
                     let alloca =
                         IntArray::new(label.clone(), size, true, inst_ref.get_int_init().clone());
                     let last = func.as_ref().stack_addr.front().unwrap();
--- src/ir/instruction/alloca.rs
+++ src/ir/instruction/alloca.rs
@@ -8,21 +8,18 @@ impl ObjPool<Inst> {
     /// * 'length' - 要申请的数组的长度
     /// # Returns
     /// 构造好的数组指令，其值为Intptr
-    pub fn make_int_array(&mut self, length: ObjPtr<Inst>, init: Vec<i32>) -> ObjPtr<Inst> {
+    pub fn make_int_array(&mut self, length: i32, init: Vec<i32>) -> ObjPtr<Inst> {
         // 正确性检查
         // 数组长度必须为整数
-        let ir_type = length.as_ref().get_ir_type();
-        debug_assert!(ir_type == IrType::Int || ir_type == IrType::ConstInt);
 
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::IntPtr,
-            InstKind::Alloca,
-            vec![length],
+            InstKind::Alloca(length),
+            vec![],
         ));
         inst.as_mut().set_int_init(init);
 
         // 设置use list
-        length.as_mut().add_user(inst.as_ref());
         inst
     }
 
@@ -32,21 +29,15 @@ impl ObjPool<Inst> {
     /// * 'length' - 要申请的数组的长度
     /// # Returns
     /// 构造好的数组指令，其值为Floatptr
-    pub fn make_float_array(&mut self, length: ObjPtr<Inst>, init: Vec<f32>) -> ObjPtr<Inst> {
-        // 正确性检查
-        // 数组长度必须为整数
-        let ir_type = length.as_ref().get_ir_type();
-        debug_assert!(ir_type == IrType::Int || ir_type == IrType::ConstInt);
-
+    pub fn make_float_array(&mut self, length: i32, init: Vec<f32>) -> ObjPtr<Inst> {
         let inst = self.put(Inst::new(
             crate::ir::ir_type::IrType::FloatPtr,
-            InstKind::Alloca,
-            vec![length],
+            InstKind::Alloca(length),
+            vec![],
         ));
         inst.as_mut().set_float_init(init);
 
         // 设置use list
-        length.as_mut().add_user(inst.as_ref());
         inst
     }
 }
@@ -54,7 +45,7 @@ impl ObjPool<Inst> {
 impl Inst {
     /// 设置整型数组的初始值,只允许在初始化的时候调用
     pub fn set_int_init(&mut self, init: Vec<i32>) {
-        if let InstKind::Alloca = self.get_kind() {
+        if let InstKind::Alloca(_) = self.get_kind() {
             if let IrType::IntPtr = self.get_ir_type() {
             } else {
                 unreachable!("Inst::set_int_init")
@@ -69,7 +60,7 @@ impl Inst {
 
     /// 设置浮点型数组的初始值,只允许在初始化的时候调用
     pub fn set_float_init(&mut self, init: Vec<f32>) {
-        if let InstKind::Alloca = self.get_kind() {
+        if let InstKind::Alloca(_) = self.get_kind() {
             if let IrType::FloatPtr = self.get_ir_type() {
             } else {
                 unreachable!("Inst::set_float_init")
@@ -85,7 +76,7 @@ impl Inst {
     /// 获得整型数组的初始值
     pub fn get_int_init(&self) -> &Vec<i32> {
         // 正确性检查
-        if let InstKind::Alloca = self.get_kind() {
+        if let InstKind::Alloca(_) = self.get_kind() {
             debug_assert!(self.get_ir_type() == IrType::IntPtr);
         } else {
             unreachable!("Inst::get_int_init")
@@ -97,7 +88,7 @@ impl Inst {
     /// 获得浮点型数组的初始值
     pub fn get_float_init(&self) -> &Vec<f32> {
         // 正确性检查
-        if let InstKind::Alloca = self.get_kind() {
+        if let InstKind::Alloca(_) = self.get_kind() {
             debug_assert!(self.get_ir_type() == IrType::FloatPtr);
         } else {
             unreachable!("Inst::get_float_init")
@@ -107,34 +98,13 @@ impl Inst {
     }
 
     /// 获得数组长度
-    pub fn get_array_length(&self) -> ObjPtr<Inst> {
+    pub fn get_array_length(&self) -> i32 {
         // 正确性检查
-        if let InstKind::Alloca = self.get_kind() {
+        if let InstKind::Alloca(length) = self.get_kind() {
             debug_assert!(self.user.get_operands_size() == 1);
+            length
         } else {
             unreachable!("Inst::get_array_length")
         }
-
-        self.user.get_operand(0)
-    }
-
-    /// 设置数组长度
-    pub fn set_array_length(&mut self, length: ObjPtr<Inst>) {
-        // 正确性检查
-        if let InstKind::Alloca = self.get_kind() {
-            debug_assert!(self.user.get_operands_size() == 1);
-            // 数组长度必须为整数或整数常量
-            let ir_type = length.as_ref().get_ir_type();
-            debug_assert!(ir_type == IrType::Int || ir_type == IrType::ConstInt);
-        } else {
-            unreachable!("Inst::set_array_length")
-        }
-
-        // 设置use list
-        let old_length = self.get_array_length();
-        old_length.as_mut().remove_user(self);
-        length.as_mut().add_user(self);
-
-        self.user.set_operand(0, length);
     }
 }
