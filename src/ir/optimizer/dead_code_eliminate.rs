4260aa3061da1de64613b8089a04ca83b9976ece
LJZ <jz.long@qq.com>
2023-06-16 20:26:26 +0800

    实现phi优化和死代码消除并完成初步验证，现在能正常运行到用例51

--- src/ir/optimizer/dead_code_eliminate.rs
+++ src/ir/optimizer/dead_code_eliminate.rs
@@ -8,12 +8,32 @@
 //! 4. br指令
 //! 5. Head指令
 
+use std::collections::HashSet;
+
 use crate::{
     ir::{basicblock::BasicBlock, instruction::InstKind},
     utility::ObjPtr,
 };
 
-pub fn dead_code_eliminate(end_bb: ObjPtr<BasicBlock>) {}
+pub fn dead_code_eliminate(end_bb: ObjPtr<BasicBlock>) {
+    let mut changed = true;
+    while changed {
+        changed = false;
+        let mut visited = HashSet::new();
+        let mut queue = Vec::new();
+        queue.insert(0, end_bb);
+        while let Some(bb) = queue.pop() {
+            if visited.contains(&bb) {
+                continue;
+            }
+            visited.insert(bb);
+            changed |= eliminate_bb_inst(bb);
+            for pred in bb.get_up_bb() {
+                queue.insert(0, pred.clone());
+            }
+        }
+    }
+}
 
 fn eliminate_bb_inst(bb: ObjPtr<BasicBlock>) -> bool {
     if bb.is_empty() {
@@ -38,9 +58,8 @@ fn eliminate_bb_inst(bb: ObjPtr<BasicBlock>) -> bool {
                     continue;
                 }
             }
-        } else {
-            inst = inst.get_next();
         }
+        inst = inst.get_next();
     }
 
     changed
