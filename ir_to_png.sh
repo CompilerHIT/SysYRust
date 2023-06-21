ad4e1024aeeaa4682a10efb9ddfc7ba42e3f125a
LJZ <jz.long@qq.com>
2023-06-21 21:00:24 +0800

    纠正ir_to_png.sh的bug

--- ir_to_png.sh
+++ ir_to_png.sh
@@ -7,15 +7,15 @@ if [ ! -d "png" ]; then
 fi
 
 cp dump.ll png
+cp dump_opt.ll png
 cd png
 
 opt -dot-cfg dump.ll -disable-output -enable-new-pm=0 -opaque-pointers
 
-for file in *.dot; do
+for file in .*.dot; do
 	dot -Tpng "$file" -o "${file%.dot}.png"
 done
 
-cp dump_opt.ll png
 opt -dot-cfg dump_opt.ll -disable-output -enable-new-pm=0 -opaque-pointers
 
 for file in *.dot; do
