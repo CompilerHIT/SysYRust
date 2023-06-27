88b2972e7992e034d7cd21b4af0517baf4b0afc2
cncsmonster <cncsmonster@163.com>
2023-06-27 15:26:12 +0800

    增加biheap

--- src/backend/regalloc/opt_gc_alloc2.rs
+++ src/backend/regalloc/opt_gc_alloc2.rs
@@ -22,15 +22,8 @@ use super::{
 };
 
 pub struct Allocator {
-    pub regs: LinkedList<Reg>,                 //所有虚拟寄存器的列表
-    pub colors: HashMap<i32, i32>,             // 保存着色结果
-    pub costs_reg: HashMap<Reg, f32>,          //记录虚拟寄存器的使用次数(作为代价)
-    pub availables: HashMap<Reg, RegUsedStat>, // 保存每个点的可用寄存器集合
-    pub nums_neighbor_color: HashMap<Reg, HashMap<i32, i32>>,
-    pub ends_index_bb: HashMap<(i32, ObjPtr<BB>), HashSet<Reg>>,
-    pub interference_regs: HashSet<Reg>,
-    pub interference_graph: HashMap<Reg, LinkedList<Reg>>, //记录冲突
-    pub spillings: HashSet<i32>,                           //记录溢出寄存器
+    pub to_color: LinkedList<Reg>, //待着色寄存器
+    pub to_save: LinkedList<Reg>,  //待恢复寄存器
 }
 
 impl Allocator {
