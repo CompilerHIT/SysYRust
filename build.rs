1b44c32c7f347492fd63ba0756fd9fcc2ea0e410
Dak <1025707920@qq.com>
2023-06-30 15:45:18 +0800

    change path

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ extern crate lalrpop;
 
 fn main() {
     lalrpop::process_root().unwrap();
-    println!("cargo:rustc-link-search=./target/debug/");
+    println!("cargo:rustc-link-search=./deps");
 }
