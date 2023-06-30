fbbcd7fcbf88ce8fac2e7a67d4dbf0d6f90874de
XMky <3078688722@qq.com>
2023-06-30 16:27:28 +0800

    rm mv or s&l to same

--- src/backend/opt/clear_pass.rs
+++ src/backend/opt/clear_pass.rs
@@ -5,17 +5,62 @@ impl BackendPass {
         self.module.func_map.iter().for_each(|(_, func)| {
             if !func.is_extern {
                 func.blocks.iter().for_each(|block| {
-                    self.rm_mv_same(*block);
+                    self.rm_useless(*block);
                 })
             }
         });
     }
 
-    fn rm_mv_same(&self, block: ObjPtr<BB>) {
-        for inst in block.insts {
-            if inst.get_type() == InstrsType::OpReg(SingleOp::IMv) || inst.get_type() == InstrsType::OpReg(SingleOp::FMv) {
-                // if inst.get_dst() == 
+    fn rm_useless(&self, block: ObjPtr<BB>) {
+        let mut index = 0;
+        loop {
+            if index >= block.insts.len() {
+                break;
             }
-        }    
+            let inst = block.insts[index];
+            if self.is_mv_same(inst) {
+                block.as_mut().insts.remove(index);
+                continue;
+            }
+            if index > 0 {
+                let prev_inst = block.insts[index - 1];
+                if self.is_sl_same(inst, prev_inst) {
+                    block.as_mut().insts.remove(index);
+                    continue;
+                }
+                if self.is_sl_same_offset(inst, prev_inst) {
+                    inst.as_mut().replace_kind(InstrsType::OpReg(SingleOp::Mv));
+                    inst.as_mut().replace_op(vec![inst.get_dst().clone(), prev_inst.get_dst().clone()]);
+                    index += 1;
+                    continue;
+                }
+            }
+            index += 1;
+        }
+    }
+
+    fn is_mv_same(&self, inst: ObjPtr<LIRInst>) -> bool {
+        if inst.get_type() == InstrsType::OpReg(SingleOp::Mv) {
+            if inst.get_dst() == inst.get_lhs() {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn is_sl_same(&self, inst: ObjPtr<LIRInst>, prev_inst: ObjPtr<LIRInst>) -> bool {
+        if self.is_sl_same_offset(inst, prev_inst) && inst.get_dst() == prev_inst.get_dst() {
+            return true;
+        }
+        false
+    }
+
+    fn is_sl_same_offset(&self, inst: ObjPtr<LIRInst>, prev_inst: ObjPtr<LIRInst>) -> bool {
+        if inst.get_type() == InstrsType::LoadFromStack && prev_inst.get_type() == InstrsType::StoreToStack {
+            if inst.get_offset() == prev_inst.get_offset() {
+                return true;
+            }
+        }
+        false
     }
 }
