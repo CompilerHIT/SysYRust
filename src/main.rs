23337b7ced1f613de58cae84f8a6593422a5662f
LJZ <jz.long@qq.com>
2023-07-01 14:49:47 +0800

    修改main函数的代码结构，不影响逻辑

--- src/main.rs
+++ src/main.rs
@@ -70,10 +70,6 @@ fn run_main() {
 
     // ir优化
     sysylib::ir::optimizer_run(&mut module, (&mut pool_bb, &mut pool_inst), o1_option);
-    //dump_now(&mut module, "dump.ll");
-
-    // sysylib::ir::optimizer_run(&mut module, (&mut pool_bb, &mut pool_inst), true);
-    // dump_now(&mut module, "dump_opt.ll");
 
     let output2 = "row_asm.log";
     // 后端解析
