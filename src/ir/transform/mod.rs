a0ef721efa20cc4076de0c623c597f88cf32244c
LJZ <jz.long@qq.com>
2023-07-03 15:48:53 +0800

    修改优化使用的结构，增加ir验证过程，这个过程会降低编译器debug模式的性能，但是在使用--release之后便没有影响

--- src/ir/transform/mod.rs
+++ src/ir/transform/mod.rs
@@ -1,6 +1,5 @@
 use super::{basicblock::BasicBlock, instruction::Inst, module::Module};
 use super::{dump_now, tools::*};
-use crate::ir::transform::constant_folding::constant_folding;
 use crate::utility::ObjPool;
 
 mod constant_folding;
@@ -9,6 +8,7 @@ mod func_inline;
 mod loop_operation;
 mod phi_optimizer;
 mod simplify_cfg;
+mod verify;
 
 pub fn optimizer_run(
     module: &mut Module,
@@ -16,36 +16,39 @@ pub fn optimizer_run(
     optimize_flag: bool,
 ) {
     // 在功能点上对phi指令进行优化
-    functional_optimizer(module);
-    constant_folding(module, &mut pools, optimize_flag);
-    dead_code_eliminate::dead_code_eliminate(module, true);
+    functional_optimizer(module, &mut pools, optimize_flag);
 
     if optimize_flag {
-        // 函数内联
-        func_inline::inline_run(module, &mut pools);
-
-        // 死代码删除
-        dead_code_eliminate::dead_code_eliminate(module, true);
-
         // 简化cfg
         simplify_cfg::simplify_cfg_run(module, &mut pools);
-
-        // phi优化
-        phi_optimizer::phi_run(module);
+        functional_optimizer(module, &mut pools, optimize_flag);
 
         // 循环优化
         loop_operation::loop_optimize(module, &mut pools);
+        functional_optimizer(module, &mut pools, optimize_flag);
+
+        // 函数内联
+        func_inline::inline_run(module, &mut pools);
+        functional_optimizer(module, &mut pools, optimize_flag);
 
         // TODO: 性能优化
     }
 }
 
-fn functional_optimizer(module: &mut Module) {
+fn functional_optimizer(
+    module: &mut Module,
+    mut pools: &mut (&mut ObjPool<BasicBlock>, &mut ObjPool<Inst>),
+    optimize_flag: bool,
+) {
+    debug_assert!(verify::verify_run(module));
     // 死代码删除
-    dead_code_eliminate::dead_code_eliminate(module, true);
+    dead_code_eliminate::dead_code_eliminate(module, optimize_flag);
 
     // phi优化
     phi_optimizer::phi_run(module);
+
+    // 常量折叠
+    constant_folding::constant_folding(module, &mut pools, optimize_flag);
 
     // 全局死代码删除
     dead_code_eliminate::global_eliminate(module);
