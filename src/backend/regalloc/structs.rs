8ea7eb5d8d18bdb0a566f83d6fc0eecd809b07f9
cncsmonster <cncsmonster@163.com>
2023-07-03 14:41:38 +0800

    wait for check for hf-39 with optgc2

--- src/backend/regalloc/structs.rs
+++ src/backend/regalloc/structs.rs
@@ -80,6 +80,7 @@ impl RegUsedStat {
     pub fn is_available_freg(&self, freg: i32) -> bool {
         let freg = freg - 32;
         let mut unusable: HashSet<i32> = HashSet::from([18, 19, 20]);
+        // unusable.extend(0..20);
         if unusable.contains(&freg) {
             return false;
         }
