7b41618b90c7b66dda3cbd91c953a5796feca399
LJZ <jz.long@qq.com>
2023-07-03 15:50:53 +0800

    懒惰代码移动简化流程

--- src/ir/transform/loop_operation/licm.rs
+++ src/ir/transform/loop_operation/licm.rs
@@ -28,18 +28,6 @@ pub fn licm_run(
             flag |= changed;
         }
 
-        // 把子循环的循环不变量放到父循环的preheader中
-        loop {
-            let mut changed = false;
-            for loop_info in loop_list.get_loop_list() {
-                changed |= licm_sub_loops(loop_info.clone());
-            }
-            if !changed {
-                break;
-            }
-            flag |= changed;
-        }
-
         if !flag {
             break;
         }
@@ -58,7 +46,12 @@ fn licm_one_loop(loop_info: ObjPtr<LoopInfo>) -> bool {
         inst_process_in_bb(bb.get_head_inst(), |mut inst| {
             if is_loop_invariant(inst, loop_info) {
                 match inst.get_kind() {
-                    InstKind::Alloca(_) | InstKind::Load | InstKind::Return | InstKind::Branch => {}
+                    InstKind::Alloca(_)
+                    | InstKind::Load
+                    | InstKind::Return
+                    | InstKind::Branch
+                    | InstKind::Phi
+                    | InstKind::Call(_) => {}
                     _ => {
                         changed = true;
                         inst.move_self();
@@ -72,27 +65,15 @@ fn licm_one_loop(loop_info: ObjPtr<LoopInfo>) -> bool {
     changed
 }
 
-fn licm_sub_loops(loop_info: ObjPtr<LoopInfo>) -> bool {
-    let mut changed = false;
-    for sub_loop in loop_info.get_sub_loops().clone() {
-        let sub_preheader = sub_loop.get_preheader();
-        let mut tail_inst = loop_info.get_preheader().get_tail_inst();
-        inst_process_in_bb(sub_preheader.get_head_inst(), |mut inst| {
-            if is_loop_invariant(inst, loop_info) {
-                inst.move_self();
-                tail_inst.insert_before(inst);
-                changed = true;
-            }
-        });
-    }
-    changed
-}
-
 fn is_loop_invariant(inst: ObjPtr<Inst>, loop_info: ObjPtr<LoopInfo>) -> bool {
     inst.get_operands().iter().all(|op| {
         op.is_global_var()
-            || !(loop_info.is_in_current_loop(&op.get_parent_bb())
-                && op.get_parent_bb() != loop_info.get_preheader())
+            || if let InstKind::Parameter = op.get_kind() {
+                true
+            } else {
+                false
+            }
+            || !loop_info.is_in_loop(&op.get_parent_bb())
             || match op.get_kind() {
                 InstKind::ConstInt(_) | InstKind::ConstFloat(_) => true,
                 _ => false,
