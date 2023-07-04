e3fa7fae3ef4933f54b686add775a1f60b332902
XMky <3078688722@qq.com>
2023-07-04 22:31:37 +0800

    add backend opt button

--- src/main.rs
+++ src/main.rs
@@ -80,5 +80,6 @@ fn run_main() {
     let output2 = "row_asm.log";
 
     // 后端解析
-    generate_asm(filename, output, output2, &mut AsmModule::new(module), true);
+    let is_opt = false;
+    generate_asm(filename, output, output2, &mut AsmModule::new(module), is_opt);
 }
