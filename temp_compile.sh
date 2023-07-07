f92940400c3103264ad46534f89baffc1140cc55
LJZ <jz.long@qq.com>
2023-07-07 16:17:21 +0800

    一个简单的编译脚本

---
+++ temp_compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source_folder="$1"
+source_files=$(ls "${source_folder}" | grep '\.sy$')
+
+compiler_path="./target/release/compiler"
+
+# 检测compiler_product文件夹是否存在
+if [ ! -d "compiler_product" ]; then
+	mkdir compiler_product
+fi
+
+for file in ${source_files}; do
+	echo "Compiling ${file}..."
+	${compiler_path} "${source_folder}/${file}" -o "compiler_product/${file%.*}.s" -O1
+	echo "Done!"
+done
