121fec632c9f4bb511ff182e698b7d82493d1547
LJZ <jz.long@qq.com>
2023-07-09 16:05:57 +0800

    将优化选择改为默认值

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn run_main() {
     drop(compunit);
 
     // ir优化
-    sysylib::ir::optimizer_run(&mut module, (&mut pool_bb, &mut pool_inst), true);
+    sysylib::ir::optimizer_run(&mut module, (&mut pool_bb, &mut pool_inst), o1_option);
 
     let output2 = "row_asm.log";
 
