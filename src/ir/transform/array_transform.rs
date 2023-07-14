ae11ff017943e86825cb8ba68136b9e34484b47b
LJZ <jz.long@qq.com>
2023-07-14 15:11:25 +0800

    修复数组优化跑太慢的问题

--- src/ir/transform/array_transform.rs
+++ src/ir/transform/array_transform.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     ir::{instruction::InstKind, ir_type::IrType},
     utility::ObjPtr,
@@ -66,10 +68,10 @@ fn global_inst_transform(
     inst: ObjPtr<Inst>,
 ) {
     if let InstKind::Alloca(len) = inst.get_kind() {
-        // 生成数组长度的全局变量
-        let var = make_global_array_2_var(inst, len, pools);
+        // 将相应的全局变量放入一个HashMap中，等使用到这个变量的时候再生成
+        let mut var = HashMap::new();
 
-        // 将生成的全局变量放入module中
+        // 获得数组的名字
         let array_name = module
             .get_all_var()
             .iter()
@@ -77,10 +79,8 @@ fn global_inst_transform(
             .unwrap()
             .0
             .clone();
-        var.iter()
-            .enumerate()
-            .for_each(|(i, x)| module.push_var(format!("{}_{}", array_name, i), x.clone()));
 
+        // 获得所有使用数组的gep指令
         let mut gep_user = Vec::new();
         inst.get_use_list().iter().for_each(|user| {
             if let InstKind::Load = user.get_kind() {
@@ -89,52 +89,68 @@ fn global_inst_transform(
                 unreachable!("global_inst_transform, user: {:?}", user.get_kind());
             }
         });
+
+        // 将这些gep指令的使用者的ptr替换为相应的全局变量
         for gep in gep_user.iter() {
             let index = gep.get_gep_offset();
             gep.get_use_list().clone().iter_mut().for_each(|user| {
                 debug_assert!(
                     user.get_kind() == InstKind::Store || user.get_kind() == InstKind::Load
                 );
-                user.set_ptr(var[index.get_int_bond() as usize]);
+                user.set_ptr(variable_get(
+                    inst,
+                    pools,
+                    &array_name,
+                    index.get_int_bond() as usize,
+                    &mut var,
+                ));
             })
+        }
+
+        // 将申请的全局变量放入module中
+        let var: Vec<_> = var
+            .iter()
+            .map(|(name, inst)| (name.clone(), inst.clone()))
+            .collect();
+        for (name, inst) in var {
+            module.push_var(name, inst);
         }
     } else {
         unreachable!()
     }
 }
 
-fn make_global_array_2_var(
-    inst: ObjPtr<Inst>,
-    len: i32,
+/// 从HashMap中获得相应的全局变量，如果没有，则生成
+fn variable_get(
+    array: ObjPtr<Inst>,
     pools: &mut (&mut ObjPool<BasicBlock>, &mut ObjPool<Inst>),
-) -> Vec<ObjPtr<Inst>> {
-    let mut var = Vec::new();
-    match inst.get_ir_type() {
-        IrType::IntPtr => {
-            let init_array = &inst.get_int_init().1;
-            let array_len = init_array.len();
-            for i in 0..len {
-                let init = if array_len == 0 || (i as usize > array_len - 1) {
-                    0
-                } else {
-                    init_array[i as usize].1
-                };
+    array_name: &String,
+    index: usize,
+    map: &mut HashMap<String, ObjPtr<Inst>>,
+) -> ObjPtr<Inst> {
+    let k = format!("{}_{}", array_name, index);
+    if map.contains_key(&k) {
+        map.get(&k).unwrap().clone()
+    } else {
+        if let IrType::IntPtr = array.get_ir_type() {
+            let bonding = if index > array.get_int_init().1.len() {
+                0
+            } else {
+                array.get_int_init().1[index].1
+            };
 
-                var.push(pools.1.make_global_int(init));
-            }
+            let var = pools.1.make_global_int(bonding);
+            map.insert(k, var);
+            var
+        } else {
+            let bondind = if index > array.get_float_init().1.len() {
+                0.0
+            } else {
+                array.get_float_init().1[index].1
+            };
+            let var = pools.1.make_global_float(bondind);
+            map.insert(k, var);
+            var
         }
-        IrType::FloatPtr => {
-            let init_array = &inst.get_float_init().1;
-            for i in 0..len {
-                let init = if i as usize > init_array.len() - 1 {
-                    0.0
-                } else {
-                    init_array[i as usize].1
-                };
-                var.push(pools.1.make_global_float(init));
-            }
-        }
-        _ => unreachable!("make_global_array_2_var, ir_type: {:?}", inst.get_ir_type()),
     }
-    var
 }
