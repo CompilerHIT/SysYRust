ab1d1321ced0b1a05a6cbe657d33c160238fd80d
LJZ <jz.long@qq.com>
2023-07-22 13:15:10 +0800

    增加接口

--- src/ir/ir_type/mod.rs
+++ src/ir/ir_type/mod.rs
@@ -11,3 +11,54 @@ pub enum IrType {
     BBlock,
     Parameter,
 }
+
+impl IrType {
+    pub fn is_pointer(&self) -> bool {
+        match self {
+            IrType::IntPtr | IrType::FloatPtr => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_int(&self) -> bool {
+        match self {
+            IrType::Int => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_float(&self) -> bool {
+        match self {
+            IrType::Float => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_void(&self) -> bool {
+        match self {
+            IrType::Void => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_int_ptr(&self) -> bool {
+        match self {
+            IrType::IntPtr => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_float_ptr(&self) -> bool {
+        match self {
+            IrType::FloatPtr => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_parameter(&self) -> bool {
+        match self {
+            IrType::Parameter => true,
+            _ => false,
+        }
+    }
+}
