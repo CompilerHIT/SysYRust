94fce83673912ef50033c5671aab27032234185c
LJZ <jz.long@qq.com>
2023-07-27 17:04:30 +0800

    完成scev中add指令的识别

--- src/ir/analysis/scev/mod.rs
+++ src/ir/analysis/scev/mod.rs
@@ -61,10 +61,7 @@ impl SCEVAnalyzer {
                 scev = self.analyze_binary_mul(inst, in_loop);
             }
             InstKind::ConstInt(value) | InstKind::GlobalConstInt(value) => {
-                scev = self.scevexp_pool.make_scev_int_constant(value);
-            }
-            InstKind::ConstFloat(value) | InstKind::GlobalConstFloat(value) => {
-                scev = self.scevexp_pool.make_scev_float_constant(value);
+                scev = self.scevexp_pool.make_scev_constant(value);
             }
             InstKind::Phi => {
                 scev = self.analyze_phi(inst, in_loop);
@@ -78,12 +75,100 @@ impl SCEVAnalyzer {
         scev
     }
 
+    fn check_rec_available(
+        lhs: ObjPtr<SCEVExp>,
+        rhs: ObjPtr<SCEVExp>,
+        cur_loop: ObjPtr<LoopInfo>,
+    ) -> (bool, bool) {
+        (
+            lhs.is_scev_rec() && lhs.get_in_loop().unwrap() == cur_loop,
+            rhs.is_scev_rec() && rhs.get_in_loop().unwrap() == cur_loop,
+        )
+    }
+
     fn analyze_binary_add(
         &mut self,
         inst: &ObjPtr<Inst>,
         in_loop: Option<ObjPtr<LoopInfo>>,
     ) -> ObjPtr<SCEVExp> {
-        todo!()
+        if let None = in_loop {
+            return self.scevexp_pool.make_scev_unknown(Some(*inst), None);
+        }
+
+        let cur_loop = in_loop.unwrap();
+        let lhs = self.analyze(&inst.get_lhs());
+        let rhs = self.analyze(&inst.get_rhs());
+
+        let mut lhs_rhs_op = |lhs: ObjPtr<SCEVExp>, rhs: ObjPtr<SCEVExp>| -> ObjPtr<SCEVExp> {
+            let mut new_op: Vec<ObjPtr<SCEVExp>> = Vec::new();
+            if rhs.is_scev_constant() {
+                new_op = lhs
+                    .get_operands()
+                    .iter()
+                    .map(|op| {
+                        if op.is_scev_constant() {
+                            self.scevexp_pool
+                                .make_scev_constant(op.get_scev_const() + rhs.get_scev_const())
+                        } else {
+                            self.scevexp_pool.make_scev_add_expr(*op, rhs, in_loop)
+                        }
+                    })
+                    .collect();
+            } else if rhs.get_in_loop().unwrap() != cur_loop
+                && cur_loop.is_a_sub_loop(rhs.get_in_loop().unwrap())
+            {
+                new_op = lhs
+                    .get_operands()
+                    .iter()
+                    .map(|op| self.scevexp_pool.make_scev_add_expr(*op, rhs, in_loop))
+                    .collect();
+            } else {
+                return self.scevexp_pool.make_scev_unknown(Some(*inst), in_loop);
+            }
+            self.scevexp_pool
+                .make_scev_add_rec_expr(new_op, Some(*inst), in_loop)
+        };
+
+        match Self::check_rec_available(lhs, rhs, cur_loop) {
+            (true, true) => {
+                let lhs_op = lhs.get_operands();
+                let rhs_op = rhs.get_operands();
+
+                let max_index = if lhs_op.len() > rhs_op.len() {
+                    lhs_op.len()
+                } else {
+                    rhs_op.len()
+                };
+
+                let mut new_op = Vec::new();
+
+                for index in 0..max_index {
+                    if index >= lhs_op.len() {
+                        new_op.push(rhs_op[index]);
+                    } else if index >= rhs_op.len() {
+                        new_op.push(lhs_op[index]);
+                    } else {
+                        if lhs_op[index].is_scev_constant() && rhs_op[index].is_scev_constant() {
+                            new_op.push(self.scevexp_pool.make_scev_constant(
+                                lhs_op[index].get_scev_const() + rhs_op[index].get_scev_const(),
+                            ));
+                        } else {
+                            new_op.push(self.scevexp_pool.make_scev_add_expr(
+                                lhs_op[index],
+                                rhs_op[index],
+                                in_loop,
+                            ));
+                        }
+                    }
+                }
+
+                self.scevexp_pool
+                    .make_scev_add_rec_expr(new_op, Some(*inst), in_loop)
+            }
+            (true, false) => lhs_rhs_op(lhs, rhs),
+            (false, true) => lhs_rhs_op(rhs, lhs),
+            (false, false) => self.scevexp_pool.make_scev_unknown(Some(*inst), in_loop),
+        }
     }
 
     fn analyze_binary_sub(
@@ -121,11 +206,7 @@ impl SCEVAnalyzer {
             let op = inst.get_operand(index);
             let mut parse = |parsee: ObjPtr<Inst>| -> ObjPtr<SCEVExp> {
                 if parsee.is_int_const() {
-                    self.scevexp_pool
-                        .make_scev_int_constant(parsee.get_int_bond())
-                } else if parsee.is_float_const() {
-                    self.scevexp_pool
-                        .make_scev_float_constant(parsee.get_float_bond())
+                    self.scevexp_pool.make_scev_constant(parsee.get_int_bond())
                 } else {
                     self.scevexp_pool.make_scev_unknown(Some(parsee), in_loop)
                 }
