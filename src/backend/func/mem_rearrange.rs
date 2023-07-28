5fe5ac9c749cd3bc51153a89a36351b4f0b56874
cncsmonster <cncsmonster@163.com>
2023-07-28 23:28:41 +0800

    重构module,通过f,hf验证

--- src/backend/func/mem_rearrange.rs
+++ src/backend/func/mem_rearrange.rs
@@ -1,3 +1,5 @@
+use core::time;
+
 use super::*;
 
 // rearrange slot实现 ,for module-build v3
@@ -5,7 +7,7 @@ impl Func {
     ///分析函数的栈空间的作用区间  (得到liveuse,livedef, live in,live out)
     /// 在handle overflow前使用,仅仅对于spill的指令进行分析
     pub fn calc_stackslot_interval(
-        &self,
+        func: &Func,
     ) -> (
         HashMap<ObjPtr<BB>, HashSet<StackSlot>>,
         HashMap<ObjPtr<BB>, HashSet<StackSlot>>,
@@ -19,7 +21,7 @@ impl Func {
         let mut live_uses: HashMap<ObjPtr<BB>, HashSet<StackSlot>> = HashMap::new();
 
         //计算stackslot的 live use, live def
-        for bb in self.blocks.iter() {
+        for bb in func.blocks.iter() {
             let mut live_def: HashSet<StackSlot> = HashSet::new();
             let mut live_use: HashSet<StackSlot> = HashSet::new();
             for inst in bb.insts.iter().rev() {
@@ -51,7 +53,7 @@ impl Func {
             // new in =  (old_out-def)+old_in
             // new out= [out_edge:uses]
             //更新live in
-            for bb in self.blocks.iter() {
+            for bb in func.blocks.iter() {
                 let live_in = live_ins.get_mut(bb).unwrap();
                 let def = live_defs.get(bb).unwrap();
                 let mut new_in = live_outs.get(bb).unwrap().clone();
@@ -63,7 +65,7 @@ impl Func {
                 }
             }
             //更新 live out
-            for bb in self.blocks.iter() {
+            for bb in func.blocks.iter() {
                 let live_out = live_outs.get_mut(bb).unwrap();
                 let mut new_live_out = live_out.clone();
                 for out_bb in bb.out_edge.iter() {
@@ -83,16 +85,19 @@ impl Func {
     }
     ///分析函数用到的栈空间的冲突,传入
     pub fn calc_stackslot_interef_with_rearrangable_set(
-        &mut self,
+        func: &Func,
         rearrangables: &HashSet<StackSlot>,
         live_outs: &HashMap<ObjPtr<BB>, HashSet<StackSlot>>,
-    ) -> HashMap<(StackSlot, StackSlot), i32> {
+    ) -> HashMap<StackSlot, HashSet<StackSlot>> {
         let mut interef = HashMap::new();
-        for bb in self.blocks.iter() {
+        for bb in func.blocks.iter() {
             let live_out = live_outs.get(bb).unwrap();
             let mut live_now = HashSet::new();
             live_out.iter().for_each(|sst| {
                 if rearrangables.contains(sst) {
+                    if !interef.contains_key(sst) {
+                        interef.insert(*sst, HashSet::new());
+                    }
                     live_now.insert(*sst);
                 }
             });
@@ -100,15 +105,14 @@ impl Func {
                 match inst.get_type() {
                     InstrsType::LoadFromStack => {
                         let sst = inst.get_stackslot_with_addr_size();
+
                         if rearrangables.contains(&sst) && !live_now.contains(&sst) {
+                            if !interef.contains_key(&sst) {
+                                interef.insert(sst, HashSet::new());
+                            }
                             for live in live_now.iter() {
-                                let key = if sst.get_pos() < live.get_pos() {
-                                    (sst, *live)
-                                } else {
-                                    (*live, sst)
-                                };
-                                let new_times = interef.get(&key).unwrap_or(&0) + 1;
-                                interef.insert(key, new_times);
+                                interef.get_mut(&sst).unwrap().insert(*live);
+                                interef.get_mut(live).unwrap().insert(sst);
                             }
                             live_now.insert(sst);
                         }
@@ -124,30 +128,46 @@ impl Func {
         //获取用于rearrange需要的栈空间
         interef
     }
-
+    ///统计指定栈空间使用次数
     pub fn rearrange_stack_slot(&mut self) {
         let mut all_rearrangable_stackslot: HashSet<StackSlot> = HashSet::new();
         //首先统计所有的可重排地址
-        for stackslot in self.stack_addr.iter().rev() {
-            if stackslot.get_pos() == 0 {
-                break;
-            }
-            all_rearrangable_stackslot.insert(*stackslot);
+        while self.stack_addr.back().unwrap().get_pos() != 0 {
+            all_rearrangable_stackslot.insert(self.stack_addr.pop_back().unwrap());
         }
-        let (_, _, _, live_outs) = self.calc_stackslot_interval();
-        let interef = self
-            .calc_stackslot_interef_with_rearrangable_set(&all_rearrangable_stackslot, &live_outs);
+        let (_, _, _, live_outs) = Func::calc_stackslot_interval(self);
 
-        //
-
-        //根据interef统计stackslot的使用次数,
-        //对于使用次数最多的stackslot,给它分配最新的stackslot
-        let mut new_slots: LinkedList<StackSlot> = LinkedList::new();
-        new_slots.push_back(StackSlot::new(0, 0));
+        let interef = Func::calc_stackslot_interef_with_rearrangable_set(
+            self,
+            &all_rearrangable_stackslot,
+            &live_outs,
+        );
+        let mut times: HashMap<StackSlot, i32> = HashMap::new();
+        //分析虚拟栈单元使用次数
+        analyse_each_inst(&self, &mut |inst| match inst.get_type() {
+            InstrsType::LoadFromStack | InstrsType::StoreToStack => {
+                let sst = inst.get_stackslot_with_addr_size();
+                if all_rearrangable_stackslot.contains(&sst) {
+                    let new_times = times.get(&sst).unwrap_or(&0) + 1;
+                    times.insert(sst, new_times);
+                }
+            }
+            _ => (),
+        });
+        //对于虚拟栈单元,按照使用次数从高到低进行排序
+        let mut ordered_ssts: Vec<StackSlot> = all_rearrangable_stackslot.iter().cloned().collect();
+        ordered_ssts.sort_by_cached_key(|sst| -times.get(sst).unwrap());
+        //进行重分配
+        self.spill_stack_map.clear();
 
         return;
-        //定位使用到的栈空间(计算它们之间的依赖关系)
+    }
+}
 
-        //分析栈空间的读写的传递
+pub fn analyse_each_inst(func: &Func, analyser: &mut dyn FnMut(&ObjPtr<LIRInst>)) {
+    for bb in func.blocks.iter() {
+        for inst in bb.insts.iter() {
+            analyser(inst);
+        }
     }
 }
---
+++ src/backend/module/schedule.rs
@@ -0,0 +1,30 @@
+use super::*;
+
+impl AsmModule {
+    /// 计算临时变量的个数
+    pub fn cal_tmp_var(&mut self) {
+        self.func_map.iter().for_each(|(_, func)| {
+            if !func.is_extern {
+                func.as_mut().cal_tmp_var();
+            }
+        });
+    }
+
+    /// 清除临时变量
+    pub fn clear_tmp_var(&mut self) {
+        self.func_map.iter().for_each(|(_, func)| {
+            if !func.is_extern {
+                func.as_mut().tmp_vars.clear();
+            }
+        });
+    }
+
+    /// 代码调度
+    pub fn list_scheduling_tech(&mut self) {
+        self.func_map.iter().for_each(|(_, func)| {
+            if !func.is_extern {
+                func.as_mut().list_scheduling_tech();
+            }
+        });
+    }
+}
