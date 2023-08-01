23632681fb3709aed5697f4c59315bbeb595b0fa
cncsmonster <cncsmonster@163.com>
2023-08-01 11:11:58 +0800

    重写remove unuse def,通过[f,hf]验证;确定一些rm inst接口

--- src/backend/func/rm_inst.rs
+++ src/backend/func/rm_inst.rs
@@ -27,88 +27,53 @@ impl Func {
         self.remove_unuse_def();
     }
 
+    //移除无用的store指令
+    pub fn remove_unuse_store(&mut self) {}
+
+    //针对mv的值短路
+    pub fn short_cut_mv(&mut self) {}
+
+    //针对常数计算的值短路, (优先改成mv,其次改成直接li)
+    pub fn short_cut_const_count(&mut self) {}
+
+    //针对特殊表达式进行的值短路
+    pub fn short_cut_complex_expr(&mut self) {}
+
     ///移除无用def指令
     pub fn remove_unuse_def(&mut self) {
+        //TODO,等待前端修改main的ret指令的类型为ScarlarType::Int
+        // 循环删除无用def
         loop {
-            self.calc_live_for_alloc_reg();
-            let mut if_finish = true;
+            self.calc_live_base();
+            let mut finish_flag = true;
             for bb in self.blocks.iter() {
-                let mut new_insts: Vec<ObjPtr<LIRInst>> = Vec::with_capacity(bb.insts.len());
-                let mut to_removed: HashSet<usize> = HashSet::new();
-                let mut live_now: HashSet<Reg> = HashSet::new();
-                bb.live_out.iter().for_each(|reg| {
-                    live_now.insert(*reg);
-                });
-                //标记阶段 ,标记需要清除的指令
-                for (index, inst) in bb.insts.iter().enumerate().rev() {
-                    for reg in inst.get_reg_def() {
-                        if !live_now.contains(&reg) && inst.get_type() != InstrsType::Call {
-                            to_removed.insert(index);
-                            break;
+                let mut new_insts = Vec::new();
+                Func::analyse_inst_with_live_now_backorder(*bb, &mut |inst, live_now| {
+                    match inst.get_type() {
+                        InstrsType::Call | InstrsType::Ret(_) => {
+                            new_insts.push(inst);
+                            return;
                         }
-                        live_now.remove(&reg);
+                        _ => (),
                     }
-                    if to_removed.contains(&index) {
-                        continue;
+                    let def_reg = inst.get_def_reg();
+                    if def_reg.is_none() {
+                        new_insts.push(inst);
+                        return;
                     }
-                    for reg in inst.get_reg_use() {
-                        live_now.insert(reg);
+                    let def_reg = def_reg.unwrap();
+                    if !live_now.contains(def_reg) {
+                        finish_flag = false;
+                        return;
                     }
-                }
-                //清楚阶段, 清除之前标记的指令
-                for (index, inst) in bb.insts.iter().enumerate() {
-                    if to_removed.contains(&index) {
-                        if_finish = false;
-                        log_file!(
-                            "remove_unusedef.txt",
-                            ":{}-{}:{}",
-                            self.label,
-                            bb.label,
-                            inst.to_string()
-                        );
-                        continue;
-                    }
-                    new_insts.push(*inst);
-                }
+                    new_insts.push(inst);
+                });
+                new_insts.reverse();
                 bb.as_mut().insts = new_insts;
             }
-            if if_finish {
+            if finish_flag {
                 break;
             }
         }
-
-        //TODO,等待前端修改main的ret指令的类型为ScarlarType::Int
-        // 循环删除无用def
-        // loop {
-        //     self.calc_live_base();
-        //     let mut finish_flag = true;
-        //     for bb in self.blocks.iter() {
-        //         let mut new_insts = Vec::new();
-        //         Func::analyse_inst_with_live_now_backorder(*bb, &mut |inst, live_now| {
-        //             match inst.get_type() {
-        //                 InstrsType::Call => {
-        //                     new_insts.push(inst);
-        //                     return;
-        //                 }
-        //                 _ => (),
-        //             }
-        //             let def_reg = inst.get_def_reg();
-        //             if def_reg.is_none() {
-        //                 new_insts.push(inst);
-        //                 return;
-        //             }
-        //             let def_reg = def_reg.unwrap();
-        //             if !live_now.contains(def_reg) {
-        //                 finish_flag = false;
-        //                 return;
-        //             }
-        //             new_insts.push(inst);
-        //         });
-        //         bb.as_mut().insts = new_insts;
-        //     }
-        //     if finish_flag {
-        //         break;
-        //     }
-        // }
     }
 }
--- src/backend/module/build.rs
+++ src/backend/module/build.rs
@@ -11,8 +11,9 @@ impl AsmModule {
         let obj_module = ObjPtr::new(self);
         self.build_lir(pool);
         BackendPass::new(obj_module).block_pass_pre_clear(pool);
-        
+
         self.remove_unuse_inst_pre_alloc();
+        self.print_func();
 
         //检查是否有存在name func里面没有,但是被调用了的函数
 
