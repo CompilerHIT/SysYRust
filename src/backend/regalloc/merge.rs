17e63b763b5e8ab0cfaebd18cd0ec8e10bc35d58
cncsmonster <cncsmonster@163.com>
2023-08-08 12:11:19 +0800

    把reg merge 中remove self mv操作移后以减少运行时间消耗,通过[f,hf]验证

--- src/backend/regalloc/merge.rs
+++ src/backend/regalloc/merge.rs
@@ -47,6 +47,7 @@ pub fn merge_reg_with_constraints(
             debug_assert!(interef_graph.get(r2).unwrap().contains(r1));
             return false;
         }
+        debug_assert!(r1.get_type() == r2.get_type());
         debug_assert!(!interef_graph.get(r1).unwrap().contains(r2));
         debug_assert!(!interef_graph.get(r2).unwrap().contains(r1));
         Func::print_func(ObjPtr::new(func), "rm_inst.txt");
@@ -84,7 +85,7 @@ pub fn merge_reg_with_constraints(
         new_v_links_to.extend(old_r2_inter.iter());
         new_v_links_to.remove(r1);
         new_v_links_to.remove(r2);
-        add_node_to_intereference_graph(interef_graph, &new_v, new_v_links_to);
+        add_node_to_intereference_graph(interef_graph, &new_v, new_v_links_to.clone());
         let mut new_v_constraint: HashSet<Reg> = old_r1_constraints.clone();
         new_v_constraint.extend(old_r2_constraints.iter());
         constraints.insert(new_v, new_v_constraint);
@@ -96,6 +97,13 @@ pub fn merge_reg_with_constraints(
         new_v_available.merge(&old_r2_available);
         availables.insert(new_v, new_v_available);
 
+        // //如果新节点可分配 (则合并成功)
+        // if new_v_available.num_available_regs(r1.get_type()) > new_v_links_to.len() {
+        //     func.replace_v_reg(r1, &new_v);
+        //     func.replace_v_reg(r2, &new_v);
+        //     return true;
+        // }
+
         //尝试着色
         if let Some(_) = alloc_with_v_interference_graph_and_base_available(
             &interef_graph,
@@ -106,8 +114,6 @@ pub fn merge_reg_with_constraints(
             //把func中所有寄存器通通替换
             func.replace_v_reg(r1, &new_v);
             func.replace_v_reg(r2, &new_v);
-            assert!(func.remove_self_mv());
-            func.remove_self_mv();
             return true;
         } else {
             constraints.remove(&new_v);
@@ -201,6 +207,7 @@ pub fn merge_reg_with_constraints(
     }
     //如果合并成功
     if if_merge {
+        assert!(func.remove_self_mv());
         loop {
             if let Some(alloc_stat) = alloc_with_v_interference_graph_and_base_available(
                 &interef_graph,
