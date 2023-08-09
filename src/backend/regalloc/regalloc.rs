4097035dd3ed0ce39678893b43990701247f3e4e
cncsmonster <cncsmonster@163.com>
2023-08-09 23:24:21 +0800

    删除冗余代码

--- src/backend/regalloc/regalloc.rs
+++ src/backend/regalloc/regalloc.rs
@@ -15,58 +15,6 @@ pub trait Regalloc {
     fn alloc(&mut self, func: &Func) -> FuncAllocStat;
 }
 
-pub fn count_stack_size(
-    func: &Func,
-    spillings: &HashSet<i32>,
-) -> (usize, HashMap<ObjPtr<BB>, usize>) {
-    // 遍历所有块,找到每个块中的spillings大小,返回其中大小的最大值,
-    let mut stack_size: usize = 0;
-    let mut bb_stack_sizes: HashMap<ObjPtr<BB>, usize> = HashMap::new();
-    let mut passed: HashSet<ObjPtr<BB>> = HashSet::new();
-    let mut walk: VecDeque<ObjPtr<BB>> = VecDeque::new();
-    walk.push_back(func.entry.unwrap().clone());
-    passed.insert(func.entry.unwrap());
-    // TOTEST
-    while !walk.is_empty() {
-        let cur = walk.pop_front().unwrap();
-        let mut bbspillings: HashSet<i32> = HashSet::new();
-        //println!("{}",cur.label);
-        for reg in &cur.as_ref().live_in {
-            if spillings.contains(&reg.get_id()) {
-                bbspillings.insert(reg.get_id());
-            }
-        }
-        let start = bbspillings.len() * 8;
-        bb_stack_sizes.insert(cur, start);
-        bbspillings.clear();
-        // 统计spilling数量
-        for inst in &cur.as_ref().insts {
-            for reg in inst.as_ref().get_reg_def() {
-                if spillings.contains(&reg.get_id()) {
-                    bbspillings.insert(reg.get_id());
-                }
-            }
-            for reg in inst.as_ref().get_reg_use() {
-                if spillings.contains(&reg.get_id()) {
-                    bbspillings.insert(reg.get_id());
-                }
-            }
-        }
-        if bbspillings.len() * 8 + start > stack_size {
-            stack_size = bbspillings.len() * 8 + start;
-        }
-        // 扩展未扩展的节点
-        for bb in &cur.as_ref().out_edge {
-            if passed.contains(&bb) {
-                continue;
-            }
-            passed.insert(bb.clone());
-            walk.push_back(bb.clone());
-        }
-    }
-    (spillings.len() * 8, bb_stack_sizes)
-}
-
 // 估计某个寄存器spill可能造成的冲突代价
 // 它作为某个指令的def的时候冲突代价为2
 // 作为某个指令的def以及use的时候冲突代价为2
