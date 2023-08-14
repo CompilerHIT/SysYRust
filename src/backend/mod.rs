f45016791848bb94937b34740182b516ab30eca2
cncsmonster <cncsmonster@163.com>
2023-08-14 19:45:58 +0800

    移除part fuse后final realloc功能正常

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -94,7 +94,7 @@ pub fn generate_asm(
     if is_opt {
         //最后进行一次寄存器分配与合并
         config::record_event("start merge reg");
-        // module.final_realloc(&mut pool);
+        module.final_realloc(&mut pool);
         config::record_event("finish merge reg");
 
         // 再次进行指令重排
